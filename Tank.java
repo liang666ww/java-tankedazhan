4061dba123cdef84abdb1b8270aab6fbbf52a9e6
liang_www <105441821+liang666ww@users.noreply.github.com>
2022-08-16 20:25:02 +0800

    Create Tank.java

---
+++ Tank.java
@@ -0,0 +1,60 @@
+package wlTankGame3;
+
+public class Tank {
+    private int x;//坦克的横坐标
+    private int y;//坦克的纵坐标
+    private int direct;//坦克方向 0 1 2 3
+    private int speed=1;
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    //上下左右移动方法
+    public void moveUp(){
+        y -= speed;
+    }
+
+    public void moveRight(){
+        x += speed;
+    }
+    public void moveDown(){
+        y+=speed;
+    }
+    public void moveLeft(){
+        x-=speed;
+    }
+
+    public int getDirect() {
+        return direct;
+    }
+
+    public void setDirect(int direct) {
+        this.direct = direct;
+    }
+
+    public Tank(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+}
